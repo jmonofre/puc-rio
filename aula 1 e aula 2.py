87e90fe6e7d76991856062d88edba6117e3eed6e
jmonofre <joaomarceloonofre@gmail.com>
2023-04-05 10:04:19 -0300

    Create aula 1 e aula 2.py

---
+++ aula 1 e aula 2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# nome = " joão"
+# idade = 18
+# AnoFut = 2028
+# AnoAtual = 2023
+# IdadeFuturo = AnoFut - AnoAtual + idade
+
+# print ("oi",nome)
+# print ("idade em 2028:",IdadeFuturo)
+
+PrimNome= "João Marcelo"
+UltNome = "de Onofre Fontes Teixeira"
+NomeFormato = UltNome + PrimNome
+print (NomeFormato)
+print (PrimNome[0])
+print (PrimNome [-1])
+print ("*" * (len(NomeFormato)//2) + "@" * len(UltNome)*2 )
